27051e850507ef0532a59340ba8d6c2dac89afe8
kitekii <tim720719@163.com>
2023-02-22 00:47:39 +0800

    remove lsp

--- lua/p-mason.lua
+++ lua/p-mason.lua
@@ -3,7 +3,7 @@ require("mason-lspconfig").setup({
     -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
     -- This setting has no relation with the `automatic_installation` setting.
     ensure_installed = {
-	"sumneko_lua"
+	-- "lua-ls"
     },
 
     -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
